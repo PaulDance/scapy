0a8ba957edbec67a8f1a2099b26164437e77c47a
Paul Mabileau <paulmabileau@hotmail.fr>
2021-01-08 19:59:51 +0100

    Add remaining QUIC frames as empty templates
    Enables implementating without too much copy-pasting left to do.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- scapy/layers/quic/packets/frames.py
+++ scapy/layers/quic/packets/frames.py
@@ -80,6 +80,27 @@ class AckFrame(FrameStorage):
     ]
 
 
+class ResetStreamFrame(FrameStorage):
+    """
+    RESET_STREAM Frame {
+      Type (i) = 0x04,
+      Stream ID (i),
+      Application Protocol Error Code (i),
+      Final Size (i),
+    }
+    """
+
+
+class StopSendingFrame(FrameStorage):
+    """
+    STOP_SENDING Frame {
+      Type (i) = 0x05,
+      Stream ID (i),
+      Application Protocol Error Code (i),
+    }
+    """
+
+
 class CryptoFrame(FrameStorage):
     """
     CRYPTO Frame {
@@ -96,10 +117,163 @@ class CryptoFrame(FrameStorage):
     ]
 
 
+class NewTokenFrame(FrameStorage):
+    """
+    NEW_TOKEN Frame {
+      Type (i) = 0x07,
+      Token Length (i),
+      Token (..),
+    }
+    """
+
+
+class StreamFrame(FrameStorage):
+    """
+    STREAM Frame {
+      Type (i) = 0x08..0x0f,
+      Stream ID (i),
+      [Offset (i)],
+      [Length (i)],
+      Stream Data (..),
+    }
+    """
+
+
+class MaxDataFrame(FrameStorage):
+    """
+    MAX_DATA Frame {
+      Type (i) = 0x10,
+      Maximum Data (i),
+    }
+    """
+
+
+class MaxStreamDataFrame(FrameStorage):
+    """
+    MAX_STREAM_DATA Frame {
+      Type (i) = 0x11,
+      Stream ID (i),
+      Maximum Stream Data (i),
+    }
+    """
+
+
+class MaxStreamsFrame(FrameStorage):
+    """
+    MAX_STREAMS Frame {
+      Type (i) = 0x12..0x13,
+      Maximum Streams (i),
+    }
+    """
+
+
+class DataBlockedFrame(FrameStorage):
+    """
+    DATA_BLOCKED Frame {
+      Type (i) = 0x14,
+      Maximum Data (i),
+    }
+    """
+
+
+class StreamDataBlockedFrame(FrameStorage):
+    """
+    STREAM_DATA_BLOCKED Frame {
+      Type (i) = 0x15,
+      Stream ID (i),
+      Maximum Stream Data (i),
+    }
+    """
+
+
+class StreamsBlockedFrame(FrameStorage):
+    """
+    STREAMS_BLOCKED Frame {
+      Type (i) = 0x16..0x17,
+      Maximum Streams (i),
+    }
+    """
+
+
+class NewConnectionIdFrame(FrameStorage):
+    """
+    NEW_CONNECTION_ID Frame {
+      Type (i) = 0x18,
+      Sequence Number (i),
+      Retire Prior To (i),
+      Length (8),
+      Connection ID (8..160),
+      Stateless Reset Token (128),
+    }
+    """
+
+
+class RetireConnectionIdFrame(FrameStorage):
+    """
+    RETIRE_CONNECTION_ID Frame {
+      Type (i) = 0x19,
+      Sequence Number (i),
+    }
+    """
+
+
+class PathChallengeFrame(FrameStorage):
+    """
+    PATH_CHALLENGE Frame {
+      Type (i) = 0x1a,
+      Data (64),
+    }
+    """
+
+
+class PathResponseFrame(FrameStorage):
+    """
+    PATH_RESPONSE Frame {
+      Type (i) = 0x1b,
+      Data (64),
+    }
+    """
+
+
+class ConnectionCloseFrame(FrameStorage):
+    """
+    CONNECTION_CLOSE Frame {
+      Type (i) = 0x1c..0x1d,
+      Error Code (i),
+      [Frame Type (i)],
+      Reason Phrase Length (i),
+      Reason Phrase (..),
+    }
+    """
+
+
+class HandshakeDoneFrame(FrameStorage):
+    """
+    HANDSHAKE_DONE Frame {
+      Type (i) = 0x1e,
+    }
+    """
+
+
 QUIC_FRAME_TYPES = {
     0x00: PaddingFrame,
     0x01: PingFrame,
-    0x02: AckFrame,
-    0x03: AckFrame,
+    **{i: AckFrame for i in range(0x02, 0x03 + 1)},
+    0x04: ResetStreamFrame,
+    0x05: StopSendingFrame,
     0x06: CryptoFrame,
+    0x07: NewTokenFrame,
+    **{i: StreamFrame for i in range(0x8, 0x0f + 1)},
+    0x10: MaxDataFrame,
+    0x11: MaxStreamDataFrame,
+    **{i: MaxStreamsFrame for i in range(0x12, 0x13 + 1)},
+    0x14: DataBlockedFrame,
+    0x15: StreamDataBlockedFrame,
+    **{i: StreamsBlockedFrame for i in range(0x16, 0x17 + 1)},
+    0x18: NewConnectionIdFrame,
+    0x19: RetireConnectionIdFrame,
+    0x1a: PathChallengeFrame,
+    0x1b: PathResponseFrame,
+    **{i: ConnectionCloseFrame for i in range(0x1c, 0x1d + 1)},
+    0x1e: HandshakeDoneFrame,
 }
